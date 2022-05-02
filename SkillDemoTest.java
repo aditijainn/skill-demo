4c2f413882b9fc74a8404cb48ee7d9f222f75d4d
aditijainn <aditijain45@gmail.com>
2022-05-01 23:39:10 -0700

    test that fails

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.multiply(2, 3));
+    }
+}
+
